8c6eb371c47d1ef9e45de761a70f6bed194b4591
Yaroslav Voloshin <voloshinhq@gmail.com>
2026-06-19 09:05:21 +0000

    Open booking in Cal popup

--- src/data/site.ts
+++ src/data/site.ts
@@ -13,7 +13,6 @@ export const socialLinks = (lang: Lang) => [
 ];
 
 export const ctaTelegramHref = 'https://t.me/voloshin_hq';
-export const calMeetingHref = 'https://cal.com/voloshinhq/30min';
 export const licenseHref = 'https://github.com/voloshinhq/voloshinhq.com/blob/main/LICENSE';
 
 export const ui = {
